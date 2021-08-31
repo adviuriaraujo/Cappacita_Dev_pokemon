5da3e909bc0bcfd2bcc2b74255d3dc6d4d10d06d
Iuri Araujo <a.iurir@gmail.com>
2021-08-31 12:41:15 -0300

    API Pokemon V1.2     1 - Inclui as funções:         salvarPokemon;         mostrarPokemon(id);         mostrarPokemons;         atualizarPokemon;         deletarPokemon;         batalhaPokemon;         potion;         revive
        2 - As funções já podem ser usadas junto a um banco de dados MySQL.
        3 - Já foi incluído o arquivo knexfile.js.
        4 - Já foram incluídos arquivos dataBase como referência e histórico.

---
+++ API Pokemon/src/database/knexfile.js
@@ -0,0 +1,13 @@
+//Preencher com seus dados
+//Foi escolhido o mysql2 para evitar erro de autorização e ter de fazer o downgrade na segurança
+module.exports = {
+    client: 'mysql2',
+    connection: {
+        host: '',
+        user: '',
+        password: '',
+        database: ''
+    }
+
+
+};
